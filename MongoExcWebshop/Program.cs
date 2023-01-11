1c9cd6f0117d760c9354a07ca8fef716c4e1ebfa
Katarina Ramer <katarina.ramer@iths.se>
2023-01-11 11:58:57 +0100

    changed namespace

--- MongoExcWebshop/Program.cs
+++ MongoExcWebshop/Program.cs
@@ -1,7 +1,7 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Driver;
 
-namespace MongoDBEx_local;
+namespace MongoExcWebshop;
 internal class Program
 {
     static void Main(string[] args)
@@ -10,18 +10,47 @@ internal class Program
 
         var database = dbClient.GetDatabase("Webshop");
         var collection = database.GetCollection<BsonDocument>("equipment");
+    }
+
+    void AddOneYarn()
+    {
+        MongoClient dbClient = new MongoClient("mongodb+srv://sample:Lbgp6Mpbn0D4AA9S@cluster0.nglqlri.mongodb.net/?retryWrites=true&w=majority");
+
+        var database = dbClient.GetDatabase("Webshop");
+        var collection = database.GetCollection<BsonDocument>("yarns");
 
         var document = new BsonDocument
         {
-            {"name", "Scissor" },
-            {"colour", "Pink" },
+            {"name", "" },
+            {"brand", "" },
+            {"material", "" },
+            {"colour", "" },
             {"price", 25 },
             {"qty", 5 }
         };
 
         collection.InsertOne(document);
 
-        Console.WriteLine("Document inserted!");
+        Console.WriteLine("Yarn added!");
+    }
+    void AddOneEquipment()
+    {
+        MongoClient dbClient = new MongoClient("mongodb+srv://sample:Lbgp6Mpbn0D4AA9S@cluster0.nglqlri.mongodb.net/?retryWrites=true&w=majority");
 
+        var database = dbClient.GetDatabase("Webshop");
+        var collection = database.GetCollection<BsonDocument>("equipment");
+
+        var document = new BsonDocument
+        {
+            {"name", "" },
+            {"material", "" },
+            {"colour", "" },
+            {"price", 25 },
+            {"qty", 5 }
+        };
+
+        collection.InsertOne(document);
+
+        Console.WriteLine("Equipment added!");
     }
 }
