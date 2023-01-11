514b10c4c77f3717c67e9a55741a2b0e822a71d0
Katarina Ramer <katarina.ramer@iths.se>
2023-01-11 11:42:04 +0100

    Add project files.

---
+++ MongoExcWebshop/Program.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDBEx_local;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        MongoClient dbClient = new MongoClient("mongodb+srv://sample:Lbgp6Mpbn0D4AA9S@cluster0.nglqlri.mongodb.net/?retryWrites=true&w=majority");
+
+        var database = dbClient.GetDatabase("webshop");
+        var collection = database.GetCollection<BsonDocument>("yarns");
+
+
+    }
+}
